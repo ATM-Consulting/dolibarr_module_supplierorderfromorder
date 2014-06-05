4414f58d39caa763c83c7e2da1c403e26907555c
Maxime Kohlhaas <maxime@atm-consulting.fr>
2014-06-05 10:21:18 +0200

    Modif affichage bouton

--- class/actions_supplierorderfromorder.class.php
+++ class/actions_supplierorderfromorder.class.php
@@ -1,33 +1,20 @@
-<?php 
+<?php
 class ActionsSupplierorderfromorder
-{ 
-     /** Overloading the doActions function : replacing the parent's function with the one below 
-      *  @param      parameters  meta datas of the hook (context, etc...) 
-      *  @param      object             the object you want to process (an invoice if you are in invoice module, a propale in propale's module, etc...) 
-      *  @param      action             current action (if set). Generally create or edit or null 
-      *  @return       void 
-      */ 
-    function formObjectOptions($parameters, &$object, &$action, $hookmanager) 
-    { 
-        /*print_r($parameters); 
-        echo "action: ".$action; 
-        print_r($object);*/
- 		if (in_array('ordercard',explode(':',$parameters['context'])) && $object->statut > 0)
-        {
-          ?>
-          	<a id="listeProd" class="butAction" href="<?=DOL_URL_ROOT."/custom/supplierorderfromorder/ordercustomer.php?id=".$_REQUEST['id']?>">Liste produits à commander</a>
-           <script type="text/javascript">
-				$(document).ready(function() {
-					$('#listeProd').appendTo('div.tabsAction');
-				})
-			</script>
-
-          <?
-        }
- 
-        $this->results=array('myreturn'=>$myvalue);
-        $this->resprints='A text to show';
- 
-        return 0;
-    }
+{
+	function addMoreActionsButtons($parameters, &$object, &$action, $hookmanager) 
+	{
+		global $langs;
+		$langs->load('supplierorderfromorder@supplierorderfromorder');
+		
+		if (in_array('ordercard',explode(':',$parameters['context'])) && ($object->statut == 1 || $object->statut == 2))
+		{
+			print '<div class="inline-block divButAction">';
+			print '<a class="butAction" href="' . dol_buildpath('/supplierorderfromorder/ordercustomer.php?id='.$_REQUEST['id'], 2) . '">' . $langs->trans('OrderToSuppliers') . '</a></div>';
+		}
+		
+		$this->results=array('myreturn'=>$myvalue);
+		$this->resprints='A text to show';
+		
+		return 0;
+	}
 }
