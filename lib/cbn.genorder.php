d9443254164f98e640cadc5825cd8e4a70cbb0f3
Francis Appels <francis.appels@yahoo.com>
2020-11-27 16:03:53 +0100

    Remove options SOFO_USE_NOMENCLATURE and SOFO_GET_INFOS_FROM_ORDER

--- lib/cbn.genorder.php
+++ lib/cbn.genorder.php
@@ -149,30 +149,16 @@ if ($action == 'order' && isset($_POST['valid'])) {
 					$order->add_object_linked('commande', $_REQUEST['id']);
 
 				//}
-				if($conf->global->SOFO_GET_INFOS_FROM_ORDER){
-					$order->mode_reglement_code = $commandeClient->mode_reglement_code;
-					$order->mode_reglement_id = $commandeClient->mode_reglement_id;
-					$order->cond_reglement_id = $commandeClient->cond_reglement_id;
-					$order->cond_reglement_code = $commandeClient->cond_reglement_code;
-					$order->date_livraison = $commandeClient->date_livraison;
-				}
 				$id++; //$id doit être renseigné dans tous les cas pour que s'affiche le message 'Vos commandes ont été générées'
 				$newCommande = false;
 			} else {
-								/*echo '<pre>';
+				/*echo '<pre>';
 				print_r($commandeClient);exit;*/
 
 				$order = new CommandeFournisseur($db);
 				$order->socid = $idsupplier;
 				if(!empty($projectid)){
 					$order->fk_project = $projectid;
-				}
-				if($conf->global->SOFO_GET_INFOS_FROM_ORDER){
-					$order->mode_reglement_code = $commandeClient->mode_reglement_code;
-					$order->mode_reglement_id = $commandeClient->mode_reglement_id;
-					$order->cond_reglement_id = $commandeClient->cond_reglement_id;
-					$order->cond_reglement_code = $commandeClient->cond_reglement_code;
-					$order->date_livraison = $commandeClient->date_livraison;
 				}
 
 				$id = $order->create($user);
