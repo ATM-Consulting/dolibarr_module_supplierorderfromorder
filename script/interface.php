6b1cb49f356a904cf64e5e0f5dbbef5a922e6f38
atm-gauthier <gauthier.verdol@atm-consulting.fr>
2021-11-10 17:12:00 +0100

    FIX : retrait group by fait par Kévin
    Co-authored-by: boudet jean pascal <68547382+atm-jpb@users.noreply.github.com>

--- script/interface.php
+++ script/interface.php
@@ -95,7 +95,6 @@ function _stockDetails()
 		$sql.= " AND c.entity = ".$conf->entity;
 		$sql.= " AND cd.fk_product = ".$prod->id;
 		$sql.= " AND c.fk_statut in (1,2)";
-		$sql.= " GROUP BY cd.rowid";
 
 		$r ='';
 		$result =$db->query($sql);
